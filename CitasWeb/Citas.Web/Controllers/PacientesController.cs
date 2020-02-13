263ef1688256d96a3816fb84c15ac1309cf1b2d4
Cesar <cesaryanes14@gmail.com>
2020-02-12 23:24:45 -0600

    Creando links de navegacion

---
+++ CitasWeb/Citas.Web/Controllers/PacientesController.cs
@@ -0,0 +1,44 @@
+﻿using Citas.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Citas.Web.Controllers
+{
+    public class PacientesController : Controller
+    {
+        // GET: Pacientes
+        public ActionResult Index()
+        {
+            var paciente1 = new PacienteModel();
+            paciente1.Id = 1;
+            paciente1.Nombre = "Santos Andres Yanes Quiroz";
+            paciente1.Telefono = 555664232;
+            paciente1.Direccion = "SPS";
+            paciente1.FechaNacimiento = "21 enero 1986";
+
+            var paciente2 = new PacienteModel();
+            paciente2.Id = 2;
+            paciente2.Nombre = "Carlos David Flores Aguilar";
+            paciente2.Telefono = 123664232;
+            paciente2.Direccion = "SPS";
+            paciente2.FechaNacimiento = "13 Mayo 1998";
+
+            var paciente3 = new PacienteModel();
+            paciente3.Id = 3;
+            paciente3.Nombre = "Miguel Raul Navarro Lopez";
+            paciente3.Telefono = 555664907;
+            paciente3.Direccion = "SPS";
+            paciente3.FechaNacimiento = "31 Agosto 1989";
+
+            var listadePacientes = new List<PacienteModel>();
+            listadePacientes.Add(paciente1);
+            listadePacientes.Add(paciente2);
+            listadePacientes.Add(paciente3);
+
+            return View(listadePacientes);
+        }
+    }
+}
---
+++ CitasWeb/Citas.Web/Models/PacienteModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Citas.Web.Models
+{
+    public class PacienteModel
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public int Telefono { get; set; }
+        public string Direccion { get; set; }
+        public string FechaNacimiento { get; set; }
+    }
+}
