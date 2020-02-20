c3850f57c8fed6810f8cdad7e7a853484e75ef15
Cesar <cesaryanes14@gmail.com>
2020-02-20 11:00:54 -0600

    Parte #2

--- CitasWeb/Citas.BL/Paciente.cs
+++ CitasWeb/Citas.BL/Paciente.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace Citas.Web.Models
+namespace Admon.BL
 {
-    public class PacienteModel
+   public class Paciente
     {
         public int Id { get; set; }
         public string Nombre { get; set; }
--- CitasWeb/Citas.Web/Controllers/PacientesController.cs
+++ CitasWeb/Citas.Web/Controllers/PacientesController.cs
@@ -1,4 +1,4 @@
-﻿using Citas.Web.Models;
+﻿using Admon.BL;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,33 +12,10 @@ namespace Citas.Web.Controllers
         // GET: Pacientes
         public ActionResult Index()
         {
-            var paciente1 = new PacienteModel();
-            paciente1.Id = 1;
-            paciente1.Nombre = "Santos Andres Yanes Quiroz";
-            paciente1.Telefono = 555664232;
-            paciente1.Direccion = "SPS";
-            paciente1.FechaNacimiento = "21 enero 1986";
+            var pacientesBL = new PacientesBL();
+            var listaPacientes = pacientesBL.ObtenerPaciente();
 
-            var paciente2 = new PacienteModel();
-            paciente2.Id = 2;
-            paciente2.Nombre = "Carlos David Flores Aguilar";
-            paciente2.Telefono = 123664232;
-            paciente2.Direccion = "SPS";
-            paciente2.FechaNacimiento = "13 Mayo 1998";
-
-            var paciente3 = new PacienteModel();
-            paciente3.Id = 3;
-            paciente3.Nombre = "Miguel Raul Navarro Lopez";
-            paciente3.Telefono = 555664907;
-            paciente3.Direccion = "SPS";
-            paciente3.FechaNacimiento = "31 Agosto 1989";
-
-            var listadePacientes = new List<PacienteModel>();
-            listadePacientes.Add(paciente1);
-            listadePacientes.Add(paciente2);
-            listadePacientes.Add(paciente3);
-
-            return View(listadePacientes);
+            return View(listaPacientes);
         }
     }
 }
